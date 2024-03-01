cdd2a5efe308ddaeb4202fb0f85543f4f811e0c2
Rong <ronrzhu@gmail.com>
2024-02-29 19:27:29 -0800

    Minor organization adjustment

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import main_loop
 import patterns
 import time
 
-if __name__ == "__main__":
+def main():
     while True:
         main_loop.main_loop()
         time.sleep(patterns.frames_per_second / 1)
+
+
+if __name__ == "__main__":
+    main()
