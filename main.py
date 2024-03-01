8c9ea7cc841a8707fb2271637e6e3256e2c81b99
Rong <ronrzhu@gmail.com>
2024-02-29 19:34:04 -0800

    Fixed time.sleep()

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 def main():
     while True:
         main_loop.main_loop()
-        time.sleep(patterns.frames_per_second / 1)
+        time.sleep(1 / patterns.frames_per_second)
 
 
 if __name__ == "__main__":
