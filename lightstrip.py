5b0d88cb02c6c74fd182a49973b1db2f2c992ef1
Rongruiz <ronrzhu@gmail.com>
2024-03-01 19:17:13 -0800

    Resolved PR comments
    Moved modes and assign_mode method to lightstrip.py alongside other minor changes for clarity.

--- lightstrip.py
+++ lightstrip.py
@@ -1,21 +1,75 @@
 import neopixel
-from patterns import static
+import patterns
+
+# Serial Data Modes
+modes = {
+    "AMP_SIGNAL": "1",
+    "READY_TO_SHOOT": "2",
+    "ROBOT_CONTAINS_NOTE": "3",
+    "VISION_SEES_NOTE": "4",
+    "DISABLED_WITHOUT_AUTO": "32",
+    "DISABLED_WITH_AUTO": "33",
+    "PURELY_ENABLED": "34", # None of modes 1-4 are active
+    "ROBOT_NOCODE": "31"
+}
+
 
 class LightStrip:
 
     def __init__(self, pixel_pin, pixel_count: int) -> None:
-
         # Create a neopixel for each lightstrip
         self.neopixel = neopixel.NeoPixel(pixel_pin, pixel_count, brightness=1, auto_write=False)
         self.PIXEL_COUNT = pixel_count
 
         # Colors for different patterns
-        self.primary: tuple[int, int, int] = (0, 0, 0)
+        self.primary: tuple[int, int, int] = (255, 0, 0)
         self.secondary: tuple[int, int, int] = (0, 0, 0)
 
         # Pattern function to call on every update
-        self.pattern = static
+        self.pattern_function = patterns.static
 
         # Which loopcount the pattern started on, used for patterns that require
         # Multiple loops to show its effect (railgun, rainbow)
         self.pattern_starting_loop = 0
+
+
+    # Assign colors and patterns to light strip
+    def assign_mode(self, current_mode: str, last_mode: str):
+        # No need to reassign if mode is same as previous loop
+        if current_mode == last_mode:
+            return
+        
+        self.pattern_starting_loop = patterns.loopcount
+
+        # Update color and pattern on light to display
+        if current_mode == modes["AMP_SIGNAL"]:
+            self.pattern_function = patterns.rainbow
+
+        elif current_mode == modes["READY_TO_SHOOT"]:
+            # Ready to shoot, railgun pattern (requested by drivers)
+            self.primary = (0, 255, 0)
+            self.pattern_function = patterns.railgun
+
+        elif current_mode == modes["ROBOT_CONTAINS_NOTE"]:
+            self.primary = (255, 165, 0)
+            self.pattern_function = patterns.static
+
+        elif current_mode == modes["VISION_SEES_NOTE"]:
+            self.primary = (173, 216, 230)
+            self.secondary = (255, 255, 255)
+            self.pattern_function = patterns.alternating
+
+        elif current_mode == modes["DISABLED_WITHOUT_AUTO"]:
+            # TO-DO
+            pass
+
+        elif current_mode == modes["DISABLED_WITH_AUTO"]:
+            # TO-DO
+            pass
+
+        elif current_mode == modes["PURELY_ENABLED"]:
+            self.primary = (255, 255, 255)
+            self.pattern_function = patterns.static
+        else:
+            self.primary = (255, 0, 0)
+            self.pattern_function = patterns.static
--- main_loop.py
+++ main_loop.py
@@ -33,7 +33,7 @@ robot to retrieve a note)
 
 import supervisor
 import board
-from lightstrip import LightStrip
+from lightstrip import LightStrip, modes
 import patterns
 import time
 import sys
@@ -42,8 +42,8 @@ import sys
 serial = sys.stdin
 tolerance = 2
 tolerance_count = 0
-current_mode = "31"
-last_mode = "31"
+current_mode = modes["ROBOT_NOCODE"]
+last_mode = modes["ROBOT_NOCODE"]
 
 # Create light objects
 strip1 = LightStrip(board.D5, 8)
@@ -57,7 +57,7 @@ def get_serial_data() -> str:
     # Return serial data if any
     if supervisor.serial_bytes_available:
         data = serial.readline()
-        if data != None:
+        if data:
             tolerance_count = 0
             return data.strip()
     
@@ -67,63 +67,9 @@ def get_serial_data() -> str:
     # Tolerance count is 2, therefore it will tolerate two frames/fps seconds
     if tolerance_count >= tolerance:
         tolerance_count = 0
-        return "31"
+        return modes["ROBOT_NOCODE"]
     tolerance_count += 1
     return last_mode
-
-
-# MODES
-modes = {
-    "AMP_SIGNAL": "1",
-    "READY_TO_SHOOT": "2",
-    "ROBOT_CONTAINS_NOTE": "3",
-    "VISION_SEES_NOTE": "4",
-    "DISABLED_WITHOUT_AUTO": "32",
-    "DISABLED_WITH_AUTO": "33",
-    "PURELY_ENABLED": "34" # None of modes 1-4 are active
-}
-
-
-# Assign colors and patterns to light strips
-def assign_mode(light: LightStrip):
-    # No need to reassign if mode is same as previous loop
-    if current_mode == last_mode:
-        return
-    
-    light.pattern_starting_loop = patterns.loopcount
-
-    # Update color and pattern on light to display
-    if current_mode == modes["AMP_SIGNAL"]:
-        light.pattern = patterns.rainbow
-
-    elif current_mode == modes["READY_TO_SHOOT"]:
-        # Ready to shoot, railgun pattern (requested by drivers)
-        light.primary = (0, 255, 0)
-        light.pattern = patterns.railgun
-
-    elif current_mode == modes["ROBOT_CONTAINS_NOTE"]:
-        light.primary = (255, 165, 0)
-        light.pattern = patterns.static
-
-    elif current_mode == modes["VISION_SEES_NOTE"]:
-        light.primary = (173, 216, 230)
-        light.secondary = (255, 255, 255)
-        light.pattern = patterns.alternating
-
-    elif current_mode == modes["DISABLED_WITHOUT_AUTO"]:
-        # TO-DO
-        pass
-
-    elif current_mode == modes["DISABLED_WITH_AUTO"]:
-        # TO-DO
-        pass
-
-    elif current_mode == modes["PURELY_ENABLED"]:
-        light.primary = (255, 255, 255)
-        light.pattern = patterns.static
-    else:
-        light.primary = (255, 0, 0)
-        light.pattern = patterns.static
 
 
 def main_loop():
@@ -138,8 +84,8 @@ def main_loop():
 
     # Match lights with serial data and update lights
     for lightstrip in lightstrips:
-        assign_mode(lightstrip)
-        lightstrip.pattern(lightstrip)
+        lightstrip.assign_mode(current_mode, last_mode)
+        lightstrip.pattern_function(lightstrip)
 
     last_mode = current_mode
 
