c5b2742706e7c6550158ada3291b4e37c02f906c
j0ndough <ewhimsical@gmail.com>
2024-04-02 19:29:50 -0700

    change some patterns

--- lightstrip.py
+++ lightstrip.py
@@ -79,7 +79,7 @@ class LightStrip:
 
         elif current_mode == modes["SIDE_CAM_SEES_NOTE"]:
             self.primary = colors["YELLOW"]
-            self.pattern_function = patterns.static
+            self.pattern_function = patterns.flashing
     
         elif current_mode == modes["ALL_CAMS_WORKING_DEFAULT"]:
             self.primary = colors["PURPLE"]
@@ -90,8 +90,8 @@ class LightStrip:
             self.pattern_function = patterns.wavy
 
         elif current_mode == modes["NO_CAMS_WORKING_DEFEAULT"]:
-            self.primary == colors["PURPLE"]
-            self.pattern_function = patterns.flashing
+            self.primary == colors["NO_COLOR"]
+            self.pattern_function = patterns.static
 
         elif current_mode == modes["ALL_CAMS_WORKING_CUSTOM"]:
             self.primary == colors["CYAN"]
@@ -102,8 +102,8 @@ class LightStrip:
             self.pattern_function = patterns.wavy
 
         elif current_mode == modes["NO_CAMS_WORKING_CUSTOM"]:
-            self.primary == colors["CYAN"]
-            self.pattern_function = patterns.flashing
+            self.primary == colors["NO_COLOR"]
+            self.pattern_function = patterns.static
 
         elif current_mode == modes["PURELY_ENABLED"]:
             self.primary = colors["WHITE"]
